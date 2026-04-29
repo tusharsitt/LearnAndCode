09afc7321cdc06e29afee607a802bfc3290fba88
Tushar Sharma <tushar.sharma@intimetec.com>
2026-04-29 14:50:23 +0000

    Add subtraction feature

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -1,8 +1,10 @@
 public class Calculator {
     public Calculator() {}
     public int add(int a, int b) { return a + b; }
+    public int subtract(int a, int b) { return a - b; }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("2 + 3 = " + calc.add(2, 3));
+        System.out.println("5 - 2 = " + calc.subtract(5, 2));
     }
 }
