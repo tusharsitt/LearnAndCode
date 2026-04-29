156abe99a1e884e16ae4ad03decb6fa8faddcff2
Tushar Sharma <tushar.sharma@intimetec.com>
2026-04-29 14:50:23 +0000

    Add division feature

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -2,11 +2,16 @@ public class Calculator {
     public Calculator() {}
     public int add(int a, int b) { return a + b; }
     public int subtract(int a, int b) { return a - b; }
-    public int multiply(int a, int b) { return a * a; } // BUG: should be a * b
+    public int multiply(int a, int b) { return a * a; }
+    public int divide(int a, int b) { 
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return a / b; 
+    }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("2 + 3 = " + calc.add(2, 3));
         System.out.println("5 - 2 = " + calc.subtract(5, 2));
         System.out.println("4 * 3 = " + calc.multiply(4, 3));
+        System.out.println("10 / 2 = " + calc.divide(10, 2));
     }
 }
