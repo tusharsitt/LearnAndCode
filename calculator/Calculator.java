d159e44357123f82433e790f670d04ea9748eb5a
Tushar Sharma <tushar.sharma@intimetec.com>
2026-04-29 14:50:23 +0000

    Initial commit: Add Calculator class with main

---
+++ calculator/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    public Calculator() {}
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Calculator started");
+    }
+}
