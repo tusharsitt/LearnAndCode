61f6c92b2fd3fa1967be486282b29c5a307130b4
Tushar Sharma <tushar.sharma@intimetec.com>
2026-04-29 14:50:23 +0000

    Add modulo feature

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -7,11 +7,13 @@ public class Calculator {
         if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
         return a / b; 
     }
+    public int modulo(int a, int b) { return a % b; }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         System.out.println("2 + 3 = " + calc.add(2, 3));
         System.out.println("5 - 2 = " + calc.subtract(5, 2));
         System.out.println("4 * 3 = " + calc.multiply(4, 3));
         System.out.println("10 / 2 = " + calc.divide(10, 2));
+        System.out.println("10 % 3 = " + calc.modulo(10, 3));
     }
 }
