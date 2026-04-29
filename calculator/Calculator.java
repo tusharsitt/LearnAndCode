581df30e1bd2854b24a204f4ee652c779eb3cc87
Tushar Sharma <tushar.sharma@intimetec.com>
2026-04-29 14:50:23 +0000

    Add addition feature

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -1,7 +1,8 @@
 public class Calculator {
     public Calculator() {}
+    public int add(int a, int b) { return a + b; }
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println("Calculator started");
+        System.out.println("2 + 3 = " + calc.add(2, 3));
     }
 }
